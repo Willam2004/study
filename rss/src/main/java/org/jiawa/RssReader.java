d16941bc32de27fc0d53142f4844a5ab3d99880b
Willam2004 <panguanjing@gmail.com>
2014-01-14 00:06:14 +0800

    增加了读取代码

--- rss/src/main/java/org/jiawa/RssReader.java
+++ rss/src/main/java/org/jiawa/RssReader.java
@@ -1,5 +1,9 @@
 package org.jiawa;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.URL;
 import java.util.List;
 
@@ -18,11 +22,22 @@ public class RssReader {
     public List<RssItemBean> readFromUrl(String url) {
         try{
             URL content = new URL(url);
+            InputStream is =
+                    content.openStream();
 
+            BufferedReader br
+                    = new BufferedReader(new InputStreamReader(is));
+            StringBuilder sb = new StringBuilder();
+            String r = br.readLine();
+            while (r != null) {
+                sb.append(r);
+                r = br.readLine();
+            }
+            System.out.println(sb.toString());
         }catch (Exception e){
 
         }
-
+        return null;
     }
 }
 
